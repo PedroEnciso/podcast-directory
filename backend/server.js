142a7858f0f9146d1a19ee31b6bb21e98555ad75
Pedro Enciso <penciso@ucsd.edu>
2023-07-08 10:07:46 -0700

    removed movies route

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import movies from "./api/movies.route.js";
 
 const app = express();
 
@@ -9,7 +8,6 @@ app.use(express.json());
 app.use(cors());
 
 // routes
-app.use("/api/v1/movies", movies);
 app.use("*", (req, res) => {
   res.status(404).json({ error: "Page not found" });
 });
